7db8db8f9e78a2267c8e0fdc39bb5050904c1e6b
gatieme <gatieme@163.com>
2016-01-17 14:55:45 +0800

    增加机器学习的代码...

--- machinelearning/K-Nearest-Neighbor/simple/main.py
+++ machinelearning/K-Nearest-Neighbor/simple/main.py
@@ -2,12 +2,20 @@
 import knn  
 from numpy import *   
   
-dataSet, labels = knn.createDataSet()  
-testX = array([1.2, 1.0])  
-k = 3  
-outputLabel = knn.kNNClassify(testX, dataSet, labels, 3)  
-print "Your input is:", testX, "and classified to class: ", outputLabel  
-  
-testX = array([0.1, 0.3])  
-outputLabel = knn.kNNClassify(testX, dataSet, labels, 3)  
-print "Your input is:", testX, "and classified to class: ", outputLabel  
+if __name__ == "__main__" :
+    
+    # create the dataset
+    dataSet, labels = knn.createDataSet()  
+
+    # set the K value of KNN
+    k = 3  
+    # classify using kNN 
+    ## test1 data(1.2, 1.0)
+    testX = array([1.2, 1.0])   
+
+    outputLabel = knn.kNNClassify(testX, dataSet, labels, k)  
+    print "Your input is:", testX, "and classified to class: ", outputLabel  
+    ## test1  data(0.1, 0.3)
+    testX = array([0.1, 0.3])  
+    outputLabel = knn.kNNClassify(testX, dataSet, labels, k)  
+    print "Your input is:", testX, "and classified to class: ", outputLabel  
