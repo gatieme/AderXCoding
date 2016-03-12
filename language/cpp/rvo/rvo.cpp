f07cdcfe30b25be58406a035297616310559e121
gatieme <gatieme@163.com>
2016-03-12 22:45:38 +0800

    修改了explicit的makefile...

--- language/cpp/rvo/rvo.cpp
+++ language/cpp/rvo/rvo.cpp
@@ -7,11 +7,9 @@ class Base
 public :
 	Base(int num)
 	{
-        //cout <<"111" <<endl;
 	    m_num = num;
 	}
-
-	Base(const Base &base)          //  复制构造函数...
+	Base(const Base &base)
 	{
 		cout <<"Copy constructor..." <<endl;
 	}
@@ -27,57 +25,26 @@ private :
 
 
 
-Base RetBase( )
+Base GetBase( )
 {
 	Base base(10);
-	//cout <<"返回值是对象类型, 应该调用拷贝构造函数" <<endl;
-
-
-    // with RVO -=> 0 times
-    // with `-fno-elide-constructors` -=> 1 times
-    return base;
+	cout <<"返回值是对象类型, 应该调用拷贝构造函数" <<endl;
+	return base;
 }
 
-void ArgBase(Base base)
+void GetBase(Base base)
 {
-    // with RVO -=> 0 times
-    // with `-fno-elide-constructors` -=> 1 times
-	//cout <<"参数是对象类型, 应该调用拷贝构造函数" <<endl;
-}
-
-void InitBase()
-{
-    Base b1(20);
-    //cout <<"使用一个类对象去初始化另一个类对象,　应该调用拷贝构造函数" <<endl;
-
-    // with RVO -=> 0 times
-    // with `-fno-elide-constructors` -=> 1 times
-    Base b2 = b1;
+	cout <<"参数是对象类型, 应该调用拷贝构造函数" <<endl;
 }
 
 
 int main( )
 {
-
-    cout <<"使用一个类对象去初始化另一个类对象,　应该调用拷贝构造函数" <<endl;
-    InitBase();
-    cout <<endl <<endl;
-
-    cout <<"使用一个可以隐式转换为Base类对象的整数值1来初始化, 应该调用拷贝构造函数" <<endl;
-	Base b1 = 1;
-    cout <<endl <<endl;
-
-	cout <<"返回值是对象类型, 应该调用拷贝构造函数" <<endl;
-    RetBase( );
-    cout <<endl <<endl;
-
-	cout <<"返回值是对象类型, 同时用返回值去初始化一个类对象, 应该调用两次拷贝构造函数" <<endl;
-    Base b2 = RetBase( );
-    cout <<endl <<endl;
-
-
-    cout <<"参数是对象类型, 应该调用拷贝构造函数" <<endl;
-    ArgBase(b1);
-    cout <<endl <<endl;
+	Base b1(20);
+	cout <<"使用一个类对象去初始化另一个类对象,　应该调用拷贝构造函数" <<endl;
+	Base b2 = b1;
+	//cout <<"使用一个可以隐式转换为Base类对象的整数值1来初始化, 应该调用拷贝构造函数" <<endl;
+	//Base b3 = 1;
+	b2 = GetBase( );
+	GetBase( );
 }
-
