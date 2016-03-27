2c74dbc3d19f8db4608681fafd7675cb8508d800
gatieme <gatieme@cloudstack.cloud.priv>
2016-03-27 16:35:38 +0800

    实例一：利用sigaction安装SIGINT信号...

--- system/unix/signal/test_sigaction.c
+++ system/unix/signal/test_sigaction.c
@@ -3,40 +3,51 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 #include <fcntl.h>
-
 #include <stdlib.h>
 #include <stdio.h>
+
 #include <errno.h>
 #include <string.h>
 #include <signal.h>
 
 
-#define ERR_EXIT(m) \
-    do \
-    { \
-        perror(m); \
-        exit(EXIT_FAILURE); \
-    } while(0)
-
 void handler(int sig);
+/*
+struct sigaction
+{
+
+    void     (*sa_handler)(int);
+    //void     (*sa_sigaction)(int, siginfo_t *, void *);
+    sigset_t  sa_mask;
+    int       sa_flags;
+    void     (*sa_restorer)(void);
+};
+*/
 
 int main(int argc, char *argv[])
 {
-    struct sigaction act;
+    struct sigaction    act;
+
     act.sa_handler = handler;
     sigemptyset(&act.sa_mask);
     act.sa_flags = 0;
+
     //因为不关心SIGINT上一次的struct sigaction所以，oact为NULL
     //与signal(handler,SIGINT)相同
     if (sigaction(SIGINT, &act, NULL) < 0)
-        ERR_EXIT("sigaction error\n");
+    {
+        perror("sigaction error\n");
+    }
 
     for (;;)
-        pause();
+    {
+        pause( );
+    }
+
     return 0;
 }
 
 void handler(int sig)
 {
-    printf("recv a sig=%d\n", sig);
+    printf("recv a sig = %d\n", sig);
 }
