9044a215a8b8583b76fc59e89d386a0ab50bdc96
gatieme <gatieme@163.com>
2017-03-19 00:00:26 +0800

    内敛汇编atomic_add...

---
+++ language/c/inline_asm/atomic_add.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+static __inline__ int atomic_add(int count, int i)
+{
+    __asm __volatile__
+    (
+        "lock;"
+        "addl %1, %0"
+        : "=m" (count)
+        : "ir" (i), "m" (count)
+    );
+    return count;
+}
+
+
+int main(void)
+{
+    int count = 1;
+    printf("%d", atomic_add(count, 5));
+
+    return EXIT_SUCCESS;
+}
