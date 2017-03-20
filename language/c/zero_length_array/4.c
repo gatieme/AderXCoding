199476ca40119e7a3066bb1abadb14bda4221218
gatieme <gatieme@163.com>
2017-03-20 18:26:18 +0800

    0长度数组

--- language/c/zero_length_array/4.c
+++ language/c/zero_length_array/4.c
@@ -4,8 +4,8 @@
 
 int main()
 {
-    char a[0];
-    char *b;
+    char a[0] = (char *)malloc(sizeof(char) * 2);
+    char *b = (char *)malloc(sizeof(char) * 2);
 
     printf("%d %d\n", sizeof(a), sizeof(b));
 }
