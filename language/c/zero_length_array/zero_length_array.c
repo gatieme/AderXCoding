acc38196fad4d020c964ab595acb84ef1e5e09bb
gatieme <gatieme@163.com>
2017-03-20 18:09:42 +0800

    0长度数组

--- language/c/zero_length_array/zero_length_array.c
+++ language/c/zero_length_array/zero_length_array.c
@@ -1,30 +1,99 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct str
-{
-    int len;
-    char s[0];
-};
 
-struct foo
+#define MAX_LENGTH      1024
+#define CURR_LENGTH      512
+
+//  0长度数组
+struct zero_buffer
 {
-    struct str *a;
-};
+    int     len;
+    char    data[0];
+}__attribute((packed));
+
+
+//  定长数组
+struct max_buffer
+{
+    int     len;
+    char    data[MAX_LENGTH];
+}__attribute((packed));
+
+
+//  指针数组
+struct point_buffer
+{
+    int     len;
+    char    *data;
+}__attribute((packed));
 
 int main(void)
 {
-    struct foo f = { NULL };
+    struct zero_buffer  *zbuffer = NULL;
+    struct max_buffer   *mbuffer = NULL;
+    struct point_buffer *pbuffer = NULL;
 
-    printf("sizeof(struct str) = %d\n", sizeof(struct str));
 
-    printf("before f.a->s.\n");
-    if(f.a->s)
+    // =====================
+    // 0长度数组  占用-开辟-销毁
+    // =====================
+    ///  占用
+    printf("the length of struct test1:%d\n",sizeof(struct zero_buffer));
+    ///  开辟
+    if ((zbuffer = (struct zero_buffer *)malloc(sizeof(struct zero_buffer) + sizeof(char) * CURR_LENGTH)) != NULL)
     {
-        printf("before printf f.a->s.\n");
-        printf(f.a->s);
-        printf("before printf f.a->s.\n");
+        zbuffer->len = CURR_LENGTH;
+        memcpy(zbuffer->data, "Hello World", CURR_LENGTH);
+
+
+        printf("%d, %s\n", zbuffer->len, zbuffer->data);
     }
+    ///  销毁
+    free(zbuffer);
+    zbuffer = NULL;
+
+
+    // =====================
+    // 定长数组  占用-开辟-销毁
+    // =====================
+    ///  占用
+    printf("the length of struct test2:%d\n",sizeof(struct max_buffer));
+    ///  开辟
+    if ((mbuffer = (struct max_buffer *)malloc(sizeof(struct max_buffer))) != NULL)
+    {
+        mbuffer->len = CURR_LENGTH;
+        memcpy(mbuffer->data, "Hello World", CURR_LENGTH);
+
+
+        printf("%d, %s\n", mbuffer->len, mbuffer->data);
+    }
+    /// 销毁
+    free(mbuffer);
+    mbuffer = NULL;
+
+    // =====================
+    // 指针数组  占用-开辟-销毁
+    // =====================
+    ///  占用
+    printf("the length of struct test3:%d\n",sizeof(struct point_buffer));
+    ///  开辟
+    if ((pbuffer = (struct point_buffer *)malloc(sizeof(struct point_buffer))) != NULL)
+    {
+        pbuffer->len = CURR_LENGTH;
+        if ((pbuffer->data = (char *)malloc(sizeof(char) * CURR_LENGTH)) != NULL)
+        {
+            memcpy(pbuffer->data, "Hello World", CURR_LENGTH);
+
+
+            printf("%d, %s\n", pbuffer->len, pbuffer->data);
+        }
+    }
+    /// 销毁
+    free(pbuffer->data);
+    free(pbuffer);
+    pbuffer = NULL;
+
 
     return EXIT_SUCCESS;
 }
